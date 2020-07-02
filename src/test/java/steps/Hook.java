4a67d0752ef8e423a0d024db07c40bd79321259f
Bas Neve <bastiaanneve@gmail.com>
2020-07-02 06:13:12 +0200

    Edited chromedriver location

--- src/test/java/steps/Hook.java
+++ src/test/java/steps/Hook.java
@@ -35,15 +35,15 @@ public class Hook extends BaseUtil{
 
         System.out.println("Opening the browser : Firefox");
 
-        /*System.setProperty("webdriver.firefox.marionette", "D:\\Libs\\geckodriver.exe");
+        /*System.setProperty("webdriver.firefox.marionette", "C:\\Libs\\geckodriver.exe");
         base.Driver = new FirefoxDriver();*/
 
 
         //Chrome driver
-//        System.setProperty("webdriver.chrome.driver", "/Users/karthikkk/ChromeDriver/chromedriver");
+//        System.setProperty("webdriver.chrome.driver", "C:\\Libs\\chromedriver.exe");
 //        base.Driver = new ChromeDriver();
 
-        System.setProperty("webdriver.chrome.driver", "C:/driver/Web/chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", "C:\\Libs\\chromedriver.exe");
         base.Driver = new ChromeDriver();
     }
 
